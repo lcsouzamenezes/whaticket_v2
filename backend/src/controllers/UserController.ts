e738e3b8e1ab87ab573c34850a6163ab7c7a0b7c
Dionatas Lazzari <62730023+Dionatas1114@users.noreply.github.com>
2021-11-26 22:08:27 -0300

    Update UserController.ts

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -51,7 +51,7 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
 
     return res.status(200).json(user);
   } catch (error) {
-    return res.json(error);
+    return res.status(400).json(error);
   }
 };
 
